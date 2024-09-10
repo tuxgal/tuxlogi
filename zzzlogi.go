b1c54b4bee8f906739703e25d17ce5e87ae6a047
G3 <tuxgal@Baymax.area51>
2024-09-09 17:46:29 -0700

    feat: Add Logger.*Empty(...) functions for empty logging at a given level.
    At times, we want to add one or more new lines to the logs without
    logging anything. When this needs to be tied to the configured level,
    these functions can be used to seamlessly achieve the desired end
    result.

--- zzzlogi.go
+++ zzzlogi.go
@@ -8,26 +8,36 @@ type Logger interface {
 	Trace(args ...interface{})
 	// Logs the specified arguments at Trace level using the specified format.
 	Tracef(format string, args ...interface{})
+	// Logs just a new empty line only if the logging level is at least Trace.
+	TraceEmpty()
 	// Logs the specified arguments at Debug level using the default format
 	// (i.e. separated by space).
 	Debug(args ...interface{})
 	// Logs the specified arguments at Debug level using the specified format.
 	Debugf(format string, args ...interface{})
+	// Logs just a new empty line only if the logging level is at least Debug.
+	DebugEmpty()
 	// Logs the specified arguments at Info level using the default format
 	// (i.e. separated by space).
 	Info(args ...interface{})
 	// Logs the specified arguments at Info level using the specified format.
 	Infof(format string, args ...interface{})
+	// Logs just a new empty line only if the logging level is at least Info.
+	InfoEmpty()
 	// Logs the specified arguments at Warn level using the default format
 	// (i.e. separated by space).
 	Warn(args ...interface{})
 	// Logs the specified arguments at Warn level using the specified format.
 	Warnf(format string, args ...interface{})
+	// Logs just a new empty line only if the logging level is at least Warn.
+	WarnEmpty()
 	// Logs the specified arguments at Error level using the default format
 	// (i.e. separated by space).
 	Error(args ...interface{})
 	// Logs the specified arguments at Error level using the specified format.
 	Errorf(format string, args ...interface{})
+	// Logs just a new empty line only if the logging level is at least Error.
+	ErrorEmpty()
 	// Logs the specified arguments at Fatal level using the default format
 	// (i.e. separated by space).
 	Fatal(args ...interface{})
